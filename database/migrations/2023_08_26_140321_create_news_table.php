9a2233d653c99f0b849c24f08528a433a0aab23f
DREDD <mrmagnusrus@gmail.com>
2023-08-26 18:10:40 +0300

    add model

---
+++ database/migrations/2023_08_26_140321_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->bigInteger('author_id')->unsigned()->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
